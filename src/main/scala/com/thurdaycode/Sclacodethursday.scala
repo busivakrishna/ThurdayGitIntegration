4618a6d7cc300950ff0b6d9fb35eced9c2166310
busivakrishna <busivakrishna@gmail.com>
2022-06-30 08:35:11 +0530

    initial commit

---
+++ src/main/scala/com/thurdaycode/Sclacodethursday.scala
@@ -0,0 +1,19 @@
+package com.thurdaycode
+import org.apache.spark.sql.SparkSession
+object Sclacodethursday {
+  def main(args:Array[String]): Unit= {
+    //creating spark session
+    val spark: SparkSession = SparkSession
+      .builder().master("local[*]")
+      .appName("DATA FROM AWS S3")
+      .getOrCreate()
+
+    //Only Comments in this code
+    //ctrl + shift + /
+    val name = "spark"
+    val age = 12
+
+
+  }
+
+}
